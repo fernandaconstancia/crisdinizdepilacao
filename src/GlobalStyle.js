3ecde9e8c6de6a3d6c74b299b48959fc3b4aa279
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2022-11-15 15:50:31 -0300

    style: add GlobalStyles

---
+++ src/GlobalStyle.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+    font-family: "Ubuntu", Helvetica, Sans-Serif;
+    list-style: none;
+  }
+
+  html, body {
+    background: var(--color-8);
+    height: 100vh;
+  }
+
+  :root {
+    --color-1: #6B00BA;
+    --color-2: #40006E;
+    --color-3: #9F19FF;
+    --color-4: #687028;
+    --color-5: #00D494;
+    --color-6: #C4D164;
+    --color-7: #ffffff; 
+    --color-8: #fafafa;
+    --color-9: #ADADAD;
+    --color-error: #e34034;
+
+  }
+`;
